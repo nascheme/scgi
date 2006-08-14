dfaf62bc9651fe6a446a49def749287fe3fe3478
Neil Schemenauer <nas@arctrix.com>
2006-08-14 12:08:10 -0600

    Prepare for 1.11 release.

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
+import sys
+import re
 from distutils.core import setup
 from distutils.extension import Extension
+from scgi.__init__ import __version__
+
+# Ensure that version number is correct.
+PAT = re.compile(r'^%s\b' % re.escape(__version__), re.MULTILINE)
+if not PAT.search(open("CHANGES.txt").read(400)):
+    raise AssertionError("version number mismatch in CHANGES.txt")
 
 setup(name = "scgi",
-      version = "1.10",
+      version = __version__,
       description = "A Python package for implementing SCGI servers.",
       author = "Neil Schemenauer",
       author_email = "nas@mems-exchange.org",
