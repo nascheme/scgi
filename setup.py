f36c1c676e2e47ece99370e1894291a03eec8e7c
Neil Schemenauer <nas@arctrix.com>
2006-08-23 20:42:07 -0600

    Add some pypi information.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-from distutils.core import setup
+from distutils import core
 from distutils.extension import Extension
 from scgi.__init__ import __version__
 
@@ -16,13 +16,33 @@ def _check_version_numbers():
 if 'sdist' in sys.argv[1:]:
     _check_version_numbers()
 
-setup(name = "scgi",
-      version = __version__,
-      description = "A Python package for implementing SCGI servers.",
-      author = "Neil Schemenauer",
-      author_email = "nas@mems-exchange.org",
-      url = "http://www.mems-exchange.org/software/scgi/",
-      license = "see LICENSE.txt",
-      packages = ['scgi'],
-      ext_modules = [Extension(name="scgi.passfd", sources=['scgi/passfd.c'])],
-      )
+kw = dict(
+    name = "scgi",
+    version =  __version__,
+    description =  "A Python package for implementing SCGI servers.",
+    author =  "Neil Schemenauer",
+    author_email =  "nas@arctrix.com",
+    #url =  "http://",
+    license =  "DFSG approved (see LICENSE.txt)",
+    packages =  ['scgi'],
+    ext_modules =  [Extension(name="scgi.passfd", sources=['scgi/passfd.c'])],
+    )
+
+# If we're running Python 2.3, add extra information
+if hasattr(core, 'setup_keywords'):
+    if 'classifiers' in core.setup_keywords:
+        kw['classifiers'] = ['Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'License :: DFSG approved',
+          'Intended Audience :: Developers',
+          'Operating System :: Unix',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: MacOS :: MacOS X',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          ]
+    if 'download_url' in core.setup_keywords:
+        kw['download_url'] = ('http://quixote.python.ca/releases/'
+                              'scgi-%s.tar.gz' % kw['version'])
+
+
+core.setup(**kw)
